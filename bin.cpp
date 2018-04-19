248dbd9ef27411fa29cec185c47d273afa3ab2b9
Matteo <pellandamatteo@gmail.com>
2018-04-19 17:32:22 +0200

    + - * / < e <= costruttore  e da bin a dec

--- bin.cpp
+++ bin.cpp
@@ -1,54 +1,75 @@
 #include "bin.h"
 
-bin::bin(unsigned int d){
-    for(unsigned int i=0; i<d-1; i++)
-        array[i] = 0;
-    array[d-1] = 1;
+bin::bin(int d){
+    int quoziente = d;
+    bool resto = 0;
+
+    while(quoziente != 0){
+        resto = quoziente%2;
+        array.push_back(resto);
+        quoziente = quoziente/2;
+    }
+    std::vector<bool> tmp;
+    quoziente = array.size();
+    for(unsigned int it = 0; it<quoziente; it++){
+        resto = array.back();
+        array.pop_back();
+        tmp.push_back(resto);
+    }
+
+    /* VEDERE SE IL VECTOR tmp CREA GARDBAGE */
+    array = tmp;
 }
-unsigned int bin::getDim() const { return array.size(); }
-bool bin::max(const bin& bin2) const {
-    if(array.size() >= bin2.getDim())
-        return true;
-    else
-        return false;
-}
+
+
 void bin::stampa() const{
     for(unsigned int i=0; i<array.size(); i++)
         std::cout<<array[i];
-    std::cout<<std::endl;
+    //std::cout<<std::endl;
 }
-void bin::insert(unsigned int i, bool val){
-    array[i] = val;
-}
-
-//bool& bin::operator [](unsigned int i) const { return array[i]; }
-/*
-bin operator+ (const bin& bin1, const bin& bin2){
-    unsigned int tmp_dim;
-    unsigned int dim_min;
-
-    unsigned int t = 0;
-    bool resto = 0;
-
-    if(bin1.max(bin2))
-        tmp_dim = bin1.getDim();
-    else
-        tmp_dim = bin2.getDim();
-
-    bin out(tmp_dim);
-
-    for(int i=dim_min-1; i=0; i--){
-        t = bin1[i] + bin2[i] + resto;
-        t = t%2;
-        resto = t/2;
-
-       // out[i] = t;
+int bin::base10() const{
+    int out = 0;
+    int i = 0;
+    for(std::vector<bool>::const_reverse_iterator it = array.rbegin(); it != array.rend(); ++it){
+        out = out + (*it * (pow(2,i)));
+        i++;
     }
-
-    if(resto == 1){
-
-    }
-
     return out;
 }
-*/
+
+bin operator+ (const bin& bin1, const bin& bin2){
+    int a = bin1.base10();
+    int b = bin2.base10();
+    bin out(a+b);
+    return out;
+}
+bin operator -(const bin& bin1, const bin& bin2){
+    int a = bin1.base10();
+    int b = bin2.base10();
+    bin out(a-b);
+    return out;
+}
+bin operator *(const bin& bin1, const bin& bin2){
+    int a = bin1.base10();
+    int b = bin2.base10();
+    bin out(a*b);
+    return out;
+}
+bin operator /(const bin& bin1, const bin& bin2){
+    int a = bin1.base10();
+    int b = bin2.base10();
+    bin out(a/b);
+    return out;
+}
+bool operator< (const bin& bin1, const bin& bin2){
+    int a = bin1.base10();
+    int b = bin2.base10();
+
+    return a<b;
+}
+bool operator<= (const bin& bin1, const bin& bin2){
+    int a = bin1.base10();
+    int b = bin2.base10();
+
+    return a<=b;
+}
--- bin.h
+++ bin.h
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <math.h>                               /* pow */
 
 class bin{
     friend bin operator+ (const bin&, const bin&);
@@ -12,18 +13,18 @@ class bin{
     friend bin operator* (const bin&, const bin&);
     friend bin operator/ (const bin&, const bin&);
 
+    friend bool operator< (const bin&, const bin&);
+    friend bool operator<= (const bin&, const bin&);
 private:
     std::vector<bool> array;
 public:
-    bin(unsigned int=1);
+    //bin();
+    bin(int = 1);                                 /* costruttore che dato in input un valore base 10 lo trasforma in base 2 */
+
 
     /* metodi ausiliari */
-    unsigned int getDim() const;
     void stampa() const;
-    void insert(unsigned int, bool);
-    bool max(const bin& vet2) const;
-
-    //bool& operator[](unsigned int) const;
+    int base10() const;
 };
 
 bin operator+ (const bin&, const bin&);
@@ -31,4 +32,7 @@ bin operator- (const bin&, const bin&);
 bin operator* (const bin&, const bin&);
 bin operator/ (const bin&, const bin&);
 
+
+bool operator< (const bin&, const bin&);
+bool operator<= (const bin&, const bin&);
 #endif // BIN_H
